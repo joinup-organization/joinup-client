48a05ca666f14ea27b42ede8418e516a6573206c
Gabriel Zecchi <g.zecchi@hotmail.com>
2022-08-31 17:32:00 -0300

    chore: initiated workspace and configs

---
+++ joinin/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'joinin',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
