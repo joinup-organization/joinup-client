97fda7c403642aa835d27e1d19220540d52361c8
Caio Rodrigues <c.alvesrodrig@gmail.com>
2022-12-04 23:33:54 -0300

    feat: get user id from auth service

--- src/pages/Applied Projects/AppliedProjects.page.tsx
+++ src/pages/Applied Projects/AppliedProjects.page.tsx
@@ -1,18 +1,22 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import React, { useEffect, useState } from 'react'
 import { getUserProjectEnroll } from '../../services/project.service'
 import { IAppliedProjectsComponent } from '../../components/AppliedProjects/AppliedProjects.model'
 import { AppliedProjectsContainer, Title, ProjectContainer } from './AppliedProjects.style'
+import { AuthService } from '../../services/auth.service'
 import Appliedproject from '../../components/AppliedProjects/AppliedProjects.component'
 import AnswersSkeleton from './AppliedProjects..skeleton'
 
 const AppliedProjects = () => {
   const [appliedProjects, setProjects] = useState<IAppliedProjectsComponent[]>([])
+  const authService = new AuthService()
+  const userId = authService.getCurrentUser?.uid
 
   useEffect(() => {
     const getProjects = async () => {
-      setProjects(await getUserProjectEnroll('CZjxQBI5mSYrLW4Zn0LuZRiLXlo2'))
+      setProjects(await getUserProjectEnroll(userId!))
     }
     getProjects()
   }, [])
