c3c39c323cf21a27f15e31d73b51cb5516573829
Caio Rodrigues <c.alvesrodrig@gmail.com>
2022-12-06 00:46:15 -0300

    feat: add answer page

---
+++ src/pages/Account/Account.page.tsx
@@ -0,0 +1,27 @@
+import { IonRouterLink } from '@ionic/react'
+import React from 'react'
+import { MainTitle } from '../../global.style'
+import { AccountContainer, AccountItemContainer, ItemList } from './Account.style'
+
+const AccountPage = () => {
+  const AccountOption = ({ title, link }: { title: string, link: string }) => {
+    return (
+            <IonRouterLink routerLink={link}>
+                <AccountItemContainer>{title}</AccountItemContainer>
+            </IonRouterLink>
+    )
+  }
+
+  return (
+        <AccountContainer>
+            <MainTitle>Minha conta</MainTitle>
+            <ItemList>
+                <AccountOption title="Alterar cadastro" link=''/>
+                <AccountOption title="Entrar" link='login'/>
+                <AccountOption title="Sair" link=''/>
+            </ItemList>
+        </AccountContainer>
+  )
+}
+
+export default AccountPage
--- src/services/answers.service.ts
+++ src/services/answers.service.ts
@@ -4,3 +4,7 @@ import { IAnswer } from '../model/answers.model'
 export const getAnswers = async (userId: string) => {
   return await axiosAdapter.get<IAnswer[]>(`getAnswers?userId=${userId}`)
 }
+
+export const getAnswer = async (id: string) => {
+  return await axiosAdapter.get<IAnswer>(`getAnswer?id=${id}`)
+}
